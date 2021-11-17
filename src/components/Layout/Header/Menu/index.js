7df0a46aa024295562504c3a227349976c059afb
Karina Rodrigues <karinamelorodrigues@gmail.com>
2021-11-16 23:13:46 -0300

    Adição da página de contato

--- src/components/Layout/Header/Menu/index.js
+++ src/components/Layout/Header/Menu/index.js
@@ -1,61 +1,61 @@
-import React from 'react';
+import React from "react";
 import { graphql, Link, useStaticQuery } from "gatsby";
-import { MenuWrapper, MenuItem, SubMenuItemWrapper } from './style';
-
+import { MenuWrapper, MenuItem, SubMenuItemWrapper } from "./style";
 
 const Menu = () => {
-    const result = useStaticQuery(graphql `
-        fragment menuItemData on ContentfulMenuItem {
-            id
-            label
-            page {
-                ... on ContentfulPage{
-                    slug
-                }
-                
-                ... on ContentfulBlog{
-                    slug
-                }
-                
-            }
-         }
-        query MenuQuery {
-            contentfulMenu {
-                menuItems {
-                    ...menuItemData
-                    subMenuItems {
-                        ...menuItemData
-                    }
-                }
-            }
+  const result = useStaticQuery(graphql`
+    fragment menuItemData on ContentfulMenuItem {
+      id
+      label
+      page {
+        ... on ContentfulPage {
+          slug
         }
-    `);
 
-    return (
-        <MenuWrapper>
-        {result.contentfulMenu.menuItems.map((menuItem) => (
-            <MenuItem key={menuItem.id}>
-                {!menuItem.subMenuItems ? (
-                    <Link to={`/${menuItem.page.slug}`}>{menuItem.label}</Link> 
-                ) : ( 
-                    <SubMenuItemWrapper>
-                        <div>{menuItem.label}</div>
-                        <div>
-                            {menuItem.subMenuItems?.map((subMenuItem) => (
-                                <div key={subMenuItem.id}>
-                                    <Link to={`/${subMenuItem.page.slug}`}>
-                                        {subMenuItem.label}
-                                    </Link>
-                                    
-                                </div>
-                            ))}
-                        </div> 
-                    </SubMenuItemWrapper>
-                    )}
-            </MenuItem>
-        ))}
-      </MenuWrapper>
-    );
-}
+        ... on ContentfulBlog {
+          slug
+        }
+      }
+    }
+    query MenuQuery {
+      contentfulMenu {
+        menuItems {
+          ...menuItemData
+          subMenuItems {
+            ...menuItemData
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <MenuWrapper>
+      {result.contentfulMenu.menuItems.map((menuItem) => (
+        <MenuItem key={menuItem.id}>
+          {!menuItem.subMenuItems ? (
+            <Link to={`/${menuItem.page.slug}`}>{menuItem.label}</Link>
+          ) : (
+            <SubMenuItemWrapper>
+              <div>{menuItem.label}</div>
+              <div>
+                {menuItem.subMenuItems?.map((subMenuItem) => (
+                  <div key={subMenuItem.id}>
+                    <Link to={`/${subMenuItem.page.slug}`}>
+                      {subMenuItem.label}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </SubMenuItemWrapper>
+          )}
+        </MenuItem>
+      ))}
+      <MenuItem>
+        <Link to="/contact">Contact</Link>
+      </MenuItem>
+    </MenuWrapper>
+  );
+};
 
 export default Menu;
